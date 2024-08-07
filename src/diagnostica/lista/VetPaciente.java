0b7e2797a9687e90ef79c6ee521763f00ab0e592
bernardomerlo <bernardomerlo49@gmail.com>
2024-08-07 17:33:14 -0300

    finalizando Main e vetPaciente

--- src/diagnostica/lista/VetPaciente.java
+++ src/diagnostica/lista/VetPaciente.java
@@ -1,64 +1,69 @@
-package lista;
+package diagnostica.lista;
 
-import entidades.Paciente;
+import diagnostica.entidades.Paciente;
 
 public class VetPaciente {
 
-	private Paciente[] vet;
-	private int nElem;
+    private Paciente[] vet;
+    private int nElem;
 
-	public VetPaciente(int tamanho) {
-		this.nElem = 0;
-		this.vet = new Paciente[tamanho];
-	}
+    public VetPaciente(int tamanho) {
+        this.nElem = 0;
+        this.vet = new Paciente[tamanho];
+    }
 
-	public Paciente getPaciente(int pos) {
-		if (pos > nElem || pos < 0) {
-			return null;
-		}
-		return vet[pos];
-	}
+    public Paciente getPaciente(int pos) {
+        if (pos >= 0 && pos < this.nElem) {
+            return this.vet[pos];
+        }
+        return null;
+    }
 
-	public int pesquisa(String cpf) {
-		if (nElem > 0) {
-			for (int i = 0; i < nElem; i++) {
-				if (vet[i].getCpf().equals(cpf)) {
-					return i;
-				}
-			}
-		}
-		return -1;
-	}
+    public int pesquisa(String cpf) {
+        for (int i = 0; i < nElem; i++) {
+            if (vet[i].getCpf().equals(cpf)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	public int inserePaciente(Paciente pacienteNovo) {
-		boolean existe = false;
-		if (this.vet.length > this.nElem) {
-			for (Paciente p : this.vet) {
-				if (p != null) {
-					if (p.getCpf().equals(pacienteNovo.getCpf())) {
-						existe = true;
-					}
-				}
-			}
-			if (!existe) {
-				this.vet[nElem] = pacienteNovo;
-				this.nElem++;
-				return 0;
-			} else {
-				return -2;
-			}
-		}
-		return -1;
-	}
+    public int insere(Paciente pacienteNovo) {
+        boolean existe = false;
+        if (this.vet.length > this.nElem) {
+            for (Paciente p : this.vet) {
+                if (p != null) {
+                    if (p.getCpf().equals(pacienteNovo.getCpf())) {
+                        existe = true;
+                    }
+                }
+            }
+            if (!existe) {
+                this.vet[nElem] = pacienteNovo;
+                this.nElem++;
+                return 0;
+            } else {
+                return -2;
+            }
+        }
+        return -1;
+    }
 
-	public boolean remove(String cpf) {
-		if (nElem > 0) {
-			for (Paciente p : vet) {
-				if (p.getCpf().equals(cpf)) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    public boolean remove(String cpf) {
+        int pos = this.pesquisa(cpf);
+        if (pos == -1) {
+            return false;
+        } else {
+            this.vet[pos] = null;
+            return true;
+        }
+    }
+
+    public void mostraVetor() {
+        for (Paciente p : this.vet) {
+            if (p != null) {
+                System.out.println(p);
+            }
+        }
+    }
 }
--- src/diagnostica/main/AppClinica.java
+++ src/diagnostica/main/AppClinica.java
@@ -1,80 +1,95 @@
-package main;
+package diagnostica.main;
 
 import java.util.Scanner;
 
-import entidades.Paciente;
-import lista.VetPaciente;
+import diagnostica.entidades.Paciente;
+import diagnostica.lista.VetPaciente;
 
 public class AppClinica {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		VetPaciente vet = iniciaVet(sc);
+        VetPaciente vet = iniciaVet(sc);
 
-		int escolha = 0;
-		while (escolha != 4) {
-			mostraMenu();
-			escolha = sc.nextInt();
-			switch (escolha) {
+        int escolha = 0;
+        while (escolha != 4) {
+            mostraMenu();
+            escolha = sc.nextInt();
+            sc.nextLine();
+            switch (escolha) {
 
-			case 1: {
-				cadastra(sc, vet);
-				break;
-			}
-			case 2: {
-				consulta(sc, vet);
-				break;
-			}
-			case 3: {
-				imprimeCadastro(sc, vet);
-				break;
-			}
+                case 1: {
+                    cadastra(sc, vet);
+                    break;
+                }
+                case 2: {
+                    consulta(sc, vet);
+                    break;
+                }
+                case 3: {
+                    imprimeCadastro(vet);
+                    break;
+                }
+            }
+        }
+        sc.close();
+    }
 
-			}
+    private static void imprimeCadastro(VetPaciente vet) {
+        vet.mostraVetor();
+    }
 
-		}
-		sc.close();
-	}
+    private static void consulta(Scanner sc, VetPaciente vet) {
+        System.out.println("Digite o Cpf que deseja consultar: ");
+        String cpf = sc.nextLine();
+        int retorno = vet.pesquisa(cpf);
+        try {
+            System.out.println(vet.getPaciente(retorno).toString());
+        } catch (Exception e) {
+            System.out.println("Nao existe");
+        }
+    }
 
-	private static void imprimeCadastro(Scanner sc, VetPaciente vet) {
-		// TODO Auto-generated method stub
+    private static VetPaciente iniciaVet(Scanner sc) {
+        System.out.println("Digite a quantidade de pacientes:");
+        int tamanho = sc.nextInt();
+        return new VetPaciente(tamanho);
+    }
 
-	}
+    private static void cadastra(Scanner sc, VetPaciente vet) {
+        String nome;
+        String cpf;
+        String dataNascimento;
+        String historico;
 
-	private static void consulta(Scanner sc, VetPaciente vet) {
-		// TODO Auto-generated method stub
+        do {
+            System.out.println("Digite seu nome: ");
+            nome = sc.nextLine();
+            System.out.println("Digite seu cpf: ");
+            cpf = sc.nextLine();
+            System.out.println("Digite sua data de nascimento: ");
+            dataNascimento = sc.nextLine();
+            System.out.println("Digite seu histórico: ");
+            historico = sc.nextLine();
+            if (nome.isEmpty() || cpf.isEmpty() || dataNascimento.isEmpty() || historico.isEmpty()) {
+                System.out.println("Todos os campos são obrigatórios. Por favor, preencha todos.");
+            }
+        } while (nome.isEmpty() || cpf.isEmpty() || dataNascimento.isEmpty() || historico.isEmpty());
 
-	}
+        Paciente paciente = new Paciente(nome, cpf, dataNascimento, historico);
+        int valor = vet.insere(paciente);
 
-	private static VetPaciente iniciaVet(Scanner sc) {
-		System.out.println("Digite a quantidade de pacientes:");
-		int tamanho = sc.nextInt();
-		return new VetPaciente(tamanho);
-	}
+        System.out.println("Cadastrado com sucesso! Valor: " + valor);
+    }
 
-	private static void cadastra(Scanner sc, VetPaciente vet) {
-		System.out.println("Digite seu nome: ");
-		sc.next();
-		String nome = sc.nextLine();
-		System.out.println("Digite seu cpf: ");
-		String cpf = sc.nextLine();
-		System.out.println("Digite sua data de nascimento: ");
-		String dataNascimento = sc.nextLine();
-		System.out.println("Digite seu historico: ");
-		String historico = sc.nextLine();
-		Paciente paciente = new Paciente(nome, cpf, dataNascimento, historico);
-		int valor = vet.inserePaciente(paciente);
-		System.out.println("Cadastrado com sucesso!" + valor);
 
-	}
-
-	private static void mostraMenu() {
-		System.out.println("1- Cadastrar");
-		System.out.println("2- Consultar");
-		System.out.println("3- Imprimir Cad	astro");
-		System.out.println("4- Sair");
-		System.out.println("\nDigite sua escolha: ");
-	}
+    private static void mostraMenu() {
+        System.out.println("1- Cadastrar");
+        System.out.println("2- Consultar");
+        System.out.println("3- Imprimir Cadastro");
+        System.out.println("4- Sair");
+        System.out.println("\nDigite sua escolha: ");
+    }
 
 }
